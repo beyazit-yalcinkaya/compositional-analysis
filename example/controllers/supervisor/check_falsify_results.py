68d6e84ab24f5cb357e2ca72e0abcb293e3609e2
beyazit-yalcinkaya <beyazit.yalcinkaya@gmail.com>
2023-02-03 14:46:35 -0800

    Add halton falsification results

--- example/controllers/supervisor/check_falsify_results.py
+++ example/controllers/supervisor/check_falsify_results.py
@@ -3,11 +3,11 @@ import numpy as np
 monolithic = []
 compositional = []
 for i in range(1, 11):
-	s = np.genfromtxt("random/falsify_csvs_" + str(i) + "/scenario_results.csv", delimiter=",", names=True)
-	s1 = np.genfromtxt("random/falsify_csvs_" + str(i) + "/subscenario1_results.csv", delimiter=",", names=True)
-	s2L = np.genfromtxt("random/falsify_csvs_" + str(i) + "/subscenario2L_results.csv", delimiter=",", names=True)
-	s2S = np.genfromtxt("random/falsify_csvs_" + str(i) + "/subscenario2S_results.csv", delimiter=",", names=True)
-	s2R = np.genfromtxt("random/falsify_csvs_" + str(i) + "/subscenario2R_results.csv", delimiter=",", names=True)
+	s = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/scenario_results.csv", delimiter=",", names=True)
+	s1 = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario1_results.csv", delimiter=",", names=True)
+	s2L = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario2L_results.csv", delimiter=",", names=True)
+	s2S = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario2S_results.csv", delimiter=",", names=True)
+	s2R = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario2R_results.csv", delimiter=",", names=True)
 	monolithic.append(s["sim_steps"].sum())
 	compositional.append(s1["sim_steps"].sum() + s2L["sim_steps"].sum() + s2S["sim_steps"].sum() + s2R["sim_steps"].sum())
 	print("---- Sample", i, "----")
