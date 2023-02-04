f551f32ddcd1c8de62f70644078378a70b700325
beyazit-yalcinkaya <beyazit.yalcinkaya@gmail.com>
2023-02-03 22:31:14 -0800

    Add falsify results for example 2

--- example2/controllers/supervisor/check_falsify_results.py
+++ example2/controllers/supervisor/check_falsify_results.py
@@ -1,19 +1,45 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 monolithic = []
 compositional = []
 for i in range(1, 11):
 	s = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/scenario_results.csv", delimiter=",", names=True)
+	s0W = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario0W_results.csv", delimiter=",", names=True)
+	s0B = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario0B_results.csv", delimiter=",", names=True)
 	s1 = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario1_results.csv", delimiter=",", names=True)
 	s2L = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario2L_results.csv", delimiter=",", names=True)
 	s2S = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario2S_results.csv", delimiter=",", names=True)
 	s2R = np.genfromtxt("halton/falsify_csvs_" + str(i) + "/subscenario2R_results.csv", delimiter=",", names=True)
 	monolithic.append(s["sim_steps"].sum())
-	compositional.append(s1["sim_steps"].sum() + s2L["sim_steps"].sum() + s2S["sim_steps"].sum() + s2R["sim_steps"].sum())
+	compositional.append(s0W["sim_steps"].sum() + s0B["sim_steps"].sum() + s1["sim_steps"].sum() + s2L["sim_steps"].sum() + s2S["sim_steps"].sum() + s2R["sim_steps"].sum())
 	print("---- Sample", i, "----")
 	print("Monolithic:", s["sim_steps"].sum())
-	print("Compositional:", s1["sim_steps"].sum() + s2L["sim_steps"].sum() + s2S["sim_steps"].sum() + s2R["sim_steps"].sum())
+	print("Compositional:", s0W["sim_steps"].sum() + s0B["sim_steps"].sum() + s1["sim_steps"].sum() + s2L["sim_steps"].sum() + s2S["sim_steps"].sum() + s2R["sim_steps"].sum())
 	print("-----------------")
 
 print("Monolithic:", np.mean(monolithic), np.std(monolithic))
 print("Compositional:", np.mean(compositional), np.std(compositional))
+
+# for ss in ["scenario", "subscenario0W", "subscenario0B", "subscenario1", "subscenario2L", "subscenario2S", "subscenario2R"]:
+# 	for i in range(1, 11):
+# 		s = np.genfromtxt("random/falsify_csvs_" + str(i) + "/" + ss + "_post_conditions.csv", delimiter=",", skip_header=True)
+# 		sems = []
+# 		delta_sems = []
+# 		previous_sem = None
+# 		for i in range(2, np.size(s, axis=0)):
+# 			sem = np.std(s[:i], ddof=1, axis=0) / np.sqrt(np.size(s[:i], axis=0))
+# 			print("SEM:", sem)
+# 			# print("Rounded:", np.round(sem, 5))
+# 			sems.append(sem)
+# 			delta_sem = None
+# 			if previous_sem is not None:
+# 				delta_sem = abs(sem - previous_sem)
+# 				delta_sems.append(delta_sem)
+# 			if delta_sem is not None and np.all(delta_sem <= 0.001):
+# 				print("DELTA SEM:", delta_sem)
+# 				input("HEY")
+# 			previous_sem = sem
+# 		# sems = np.array(sems)
+# 		# plt.plot(sems)
+# 		# plt.show()
--- example2/controllers/supervisor/run_falsify_exps.sh
+++ example2/controllers/supervisor/run_falsify_exps.sh
@@ -1,25 +1,34 @@
 
-echo "---- Random ----"
-mkdir random
-for i in {1..10}
-do
-   echo "Iteration ${i}"
-   mkdir falsify_csvs
-   python3.9 analyze.py falsify random compositional 5
-   python3.9 analyze.py falsify random monolithic
-   mv falsify_csvs falsify_csvs_${i}
-   mv falsify_csvs_${i} random/
-done
+# echo "---- Random ----"
+# mkdir random
+# for i in {1..10}
+# do
+#    echo "Iteration ${i}"
+#    mkdir falsify_csvs
+#    python3.9 analyze.py falsify random compositional 5
+#    python3.9 analyze.py falsify random monolithic
+#    mv falsify_csvs falsify_csvs_${i}
+#    mv falsify_csvs_${i} random/
+# done
+
+
+# echo "---- Halton ----"
+# mkdir halton
+# for i in {1..10}
+# do
+#    echo "Iteration ${i}"
+#    mkdir falsify_csvs
+#    python3.9 analyze.py falsify halton compositional 5
+#    python3.9 analyze.py falsify halton monolithic
+#    mv falsify_csvs falsify_csvs_${i}
+#    mv falsify_csvs_${i} halton/
+# done
 
 
 echo "---- Halton ----"
-mkdir halton
-for i in {1..10}
-do
-   echo "Iteration ${i}"
-   mkdir falsify_csvs
-   python3.9 analyze.py falsify halton compositional 5
-   python3.9 analyze.py falsify halton monolithic
-   mv falsify_csvs falsify_csvs_${i}
-   mv falsify_csvs_${i} halton/
-done
+echo "Iteration 11"
+mkdir falsify_csvs
+python3.9 analyze.py falsify halton compositional 5
+python3.9 analyze.py falsify halton monolithic
+mv falsify_csvs falsify_csvs_11
+mv falsify_csvs_11 halton/
